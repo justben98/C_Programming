ab2af08f607754c20b032b601f82fd3bcf5cdf04
justech <justech@ubuntu.com>
2021-01-19 15:43:10 +0300

    First commit.

---
+++ TestProgram.c
@@ -0,0 +1,17 @@
+/* C programing
+ * This is my first program. Printing a string on the console.
+ * Date: 19th January 2021
+ * 
+ * Author: Justfier
+ */
+
+#include <stdio.h> //include input/output header file.
+
+int main ()
+{
+	//output a string.
+	printf("This is my first program. \n");
+	
+	//Upon success return...
+	return 0;
+}
